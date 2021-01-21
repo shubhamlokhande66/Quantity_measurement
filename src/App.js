2a05b8d80f51abf56d004d7fb4bfde1089dccddb
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-01-21 12:03:53 +0530

    [ Shubham] Add. Toolbar componants Created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Routing, Route, Switch } from "react-router-dom";
+import Appbar from "./component/Toolbar/Toolbar"
 
 function App() {
   return (
+    <Routing>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Switch>
+          <Route path="/appbar" component={Appbar} />
+
+      
+    </Switch>
     </div>
+    </Routing>
   );
 }
 
---
+++ src/component/Toolbar/Toolbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Toolbar.css";
+
+
+export default function Toolbar() {
+  return (
+    
+      <div className='welcomeNote'>
+           <h3 style={{color: 'white'}} > Welcome To Quantity Measurement</h3>
+      </div>
+    
+  );
+}
