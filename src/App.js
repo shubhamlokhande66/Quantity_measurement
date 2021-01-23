949585fe75ba20765d9565a03ded42c7b1f78b71
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2021-01-23 09:50:14 +0530

    [Shubham] refactor.Route part change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Routing, Route, Switch } from "react-router-dom";
-import Appbar from "./component/Toolbar/Toolbar"
-
+import Home from "./Pages/Home"
 function App() {
   return (
     <Routing>
     <div className="App">
     <Switch>
-          <Route path="/appbar" component={Appbar} />
-
+          <Route path="/home" component={Home} />
+        
       
     </Switch>
     </div>
